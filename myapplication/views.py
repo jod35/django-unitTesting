71f51f965ef91cf46b5d5ba78623c6ac95533a91
jod35 <jodestrevin@gmail.com>
2020-07-27 12:35:31 -0700

    created firts templates,views and urls

--- myapplication/views.py
+++ myapplication/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'myapplication/index.html')
+
+
+def about(request):
+    return render(request,'myapplication/about.html')
